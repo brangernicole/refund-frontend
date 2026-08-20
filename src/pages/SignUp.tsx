88c6b97fb8de1afac4fcaa12c6dea1f074213284
Wellington Oliveira <brangernicole@gmail.com>
2026-08-20 17:48:56 -0300

    Creating SignUp

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -2,18 +2,27 @@ import { useState } from "react"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
 
-export function SignIn() {
+export function SignUp() {
+  const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [passwordConfirm, setPasswordConfirm] = useState("")
   const [isLoading, setIsLoading] = useState(false)
   
   function onSubmit(e: React.FormEvent){
     e.preventDefault()
-    console.log(email, password)
+    console.log(name, email, password, passwordConfirm)
   }
   return (
   <form onSubmit={onSubmit}
     className="w-full flex flex-col gap-4">
+    <Input 
+    required 
+    legend="Nome"  
+    placeholder="Seu nome" 
+    onChange={(e) => setName(e.target.value)}
+    />  
+
     <Input 
     required 
     legend="E-mail" 
@@ -29,11 +38,20 @@ export function SignIn() {
     placeholder="..." 
     onChange={(e) => setPassword(e.target.value)}
     />  
+    <Input 
+    required 
+    legend="Confirmação da senha" 
+    type="password" 
+    placeholder="..." 
+    onChange={(e) => setPasswordConfimr(e.target.value)}
+    /> 
     <Button type="submit" isLoading={isLoading}>
-      Entrar
+      Cadastrar
     </Button>
 
-    <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">Criar conta</a>
+    <a href="/signup" className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear">
+    Já tenho uma conta
+    </a>
   </form>
   )
 }
