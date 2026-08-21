07016972269abf1fbcfac85709b936d1c4eb5d44
Wellington Oliveira <brangernicole@gmail.com>
2026-08-21 14:01:32 -0300

    Creating AppLayout

--- src/routes/EmployeeRoutes.tsx
+++ src/routes/EmployeeRoutes.tsx
@@ -2,11 +2,15 @@ import { Routes, Route } from "react-router"
 
 import { Refund } from "../pages/Refund"
 import { NotFound } from "../pages/NotFound"
+import { AppLayout } from "../components/AppLauout"
 
 export function EmployeeRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Refund />} />
+      <Route path="/" element={<AppLayout />}>
+        <Route path="/" element={<Refund />} />
+      </Route>
+
       <Route path="*" element={<NotFound />} />
     </Routes>
   )
