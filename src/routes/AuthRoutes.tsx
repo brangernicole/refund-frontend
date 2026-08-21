7384cb2062ed72fa20f9fc102f370b021ff995b3
Wellington Oliveira <brangernicole@gmail.com>
2026-08-21 13:54:07 -0300

    Creating the Refund Route

--- src/routes/AuthRoutes.tsx
+++ src/routes/AuthRoutes.tsx
@@ -6,6 +6,7 @@ import { SignIn } from "../pages/SignIn"
 import { SignUp } from "../pages/SignUp"
 import { NotFound } from "../pages/NotFound"
 
+
 export function AuthRoutes() {
   return (
     <Routes>
