6acf25bda3319c8f207bd54465f2017cf1db9dff
Wellington Oliveira <brangernicole@gmail.com>
2026-08-25 17:25:23 -0300

    Send Button

--- src/pages/Refund.tsx
+++ src/pages/Refund.tsx
@@ -3,17 +3,27 @@ import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories"
 import { Input } from "../components/Input"
 import { Select } from "../components/Select"
 import { Upload } from "../components/Upload"
+import { Button } from "../components/Button"
 
 export function Refund() {
+ const [name, setName] = useState("")
+ const [amount, setAmount] = useState("")
  const [category, setCategory] = useState("")
+ const [isLoading, setIsLoading] = useState(false)
+ const [filename, setFilename] = useState<File | null>(null)
+
+ function onSbmit(e: React.FormEvent){
+  e.preventDefault()
+  console.log(name, amount, category, filename)
+ }
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form onSubmit={onSbmit} className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
       <header>
         <h1 className="text-xl font-bold text-gray-100">Solicitação de Reembolso</h1>
         <p className="text-sm text-gray-200 mt-2 mb-4">Dados da despesa para solicitar reembolso</p>
       </header>
 
-      <Input required legend="Nome da solicitação" />
+      <Input required legend="Nome da solicitação" value={name} onChange={(e) => setName(e.target.value)} />
 
      <div className="flex gap-4">
       <Select required legend="Categoria" value={category} onChange={(e) => setCategory(e.target.value)}>
@@ -24,10 +34,12 @@ export function Refund() {
           ))}
       </Select>
 
-      <Input legend="Valor" required/>
+      <Input legend="Valor" required value={amount} onChange={(e) => setAmount(e.target.value)}/>
       </div>
     
-    <Upload />
+    <Upload filename={filename && filename.name} onChange={(e) => e.target.files && setFilename(e.target.files[0])}/>
+
+    <Button type="submit" isLoading={isLoading}>Enviar</Button>
     </form>
   )
 }
