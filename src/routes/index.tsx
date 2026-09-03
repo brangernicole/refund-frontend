e97433c37384a752dd03a18034bbe5a5a31fc490
Wellington Oliveira <brangernicole@gmail.com>
2026-09-03 17:40:56 -0300

    User Role

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,15 +7,33 @@ import { ManagerRoutes } from "./ManagerRoutes"
 import { EmployeeRoutes } from "./EmployeeRoutes"
 
 const isLoading = false
+//const session = undefined
+
+const session = {
+  user: {
+    role: "employee"
+  },
+}
 
 export function Routes() {
+  function Route(){
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />
+      case "manager":
+        return <ManagerRoutes />
+      default:
+        return <AuthRoutes />
+    }
+  }
+
   if(isLoading) {
     return <Loading />
   }
 
   return (
     <BrowserRouter> 
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   )
 }
